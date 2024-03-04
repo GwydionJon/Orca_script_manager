368e1efe582572745ee18891e5d3f14e9d8f1c38
Gwydion <daskalakis@stud.uni-heidelberg.de>
2024-03-04 11:21:04 +0100

    added tqdm to req

--- script_maker2000/tests/test_molecule_preperation.py
+++ script_maker2000/tests/test_molecule_preperation.py
@@ -7,7 +7,5 @@ def test_molecule_preperation(temp_work_dir):
     mol_prep = Molecule_Preparation(main_config_path)
 
     test_conversion = mol_prep.convert_smiles_to_sdf()
-    print(test_conversion)
     assert len(test_conversion[0]) == 30
     assert len(test_conversion[1]) == 30
-    1 / 0
